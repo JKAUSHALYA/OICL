6168ac04898eafa47d7be4146b914dfb42c5d6eb
Jayanga <jkasushalya@gmail.com>
2015-02-04 03:13:31 +0530

    Added the deployer project.

---
+++ OICL-Deployer/src/com/oicl/deployer/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.oicl.deployer.main;
+
+/**
+ * @author JAYANGA KAUSHALYA
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
